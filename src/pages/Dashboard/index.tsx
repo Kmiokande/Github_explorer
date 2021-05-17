9c8401e0e9405cdeb24f68656a6574b5320195aa
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-05-17 00:26:11 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28723539?v=4"
+            alt="Bruno"
+          />
+          <div>
+            <strong>Kmiokande/Github_explorer</strong>
+            <p>Descrição que não existe</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28723539?v=4"
+            alt="Bruno"
+          />
+          <div>
+            <strong>Kmiokande/Github_explorer</strong>
+            <p>Descrição que não existe</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28723539?v=4"
+            alt="Bruno"
+          />
+          <div>
+            <strong>Kmiokande/Github_explorer</strong>
+            <p>Descrição que não existe</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
